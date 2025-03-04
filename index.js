159a34d794b6b9fe4d924e1fc731ef3eee6af3d4
Mohammed Zoabi <zhamodi@gmail.com>
2025-03-04 19:44:09 +0200

    הגדרת שרת

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// index.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// הגדרת שרת
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
