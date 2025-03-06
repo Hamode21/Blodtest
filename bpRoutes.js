bc524cc331b6529b3d8fa180202ab7ced96f2068
Mohammed Zoabi <zhamodi@gmail.com>
2025-03-06 18:36:12 +0200

    המשך עדכון
    שורה 53: שיניתי 
    module.exports = (db) => { ל-module.exports = (sql) => { כי אנחנו משתמשים ב-mssql ולא ב-sqlite3.
    
    הוספתי async

--- bpRoutes.js
+++ bpRoutes.js
@@ -22,4 +22,67 @@ const validateMeasurement = (req, res, next) => {
     req.validatedData = { sys, dia, pul };
     next();
 };
+/**
+ * @swagger
+ * /bp/{userId}:
+ *   post:
+ *     summary: הוספת מדידת לחץ דם חדשה
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               systolic:
+ *                 type: number
+ *               diastolic:
+ *                 type: number
+ *               pulse:
+ *                 type: number
+ *               date:
+ *                 type: string
+ *                 format: date-time
+ *     responses:
+ *       201:
+ *         description: המדידה נשמרה
+ *       400:
+ *         description: שגיאה בנתונים
+ */
+module.exports = (sql) => { // שינוי מ-db ל-sql
+    router.post('/bp/:userId', validateMeasurement, async (req, res) => { // הוספת async
+        const userId = req.params.userId;
+        const { sys, dia, pul } = req.validatedData;
+        const { date } = req.body;
 
+        const newMeasurement = {
+            userId,
+            systolic: sys,
+            diastolic: dia,
+            pulse: pul,
+            date: date || new Date().toISOString(),
+        };
+
+        try {
+            const pool = await sql.connect();
+            await pool.request()
+                .input('userId', sql.NVarChar, userId)
+                .input('systolic', sql.Int, sys)
+                .input('diastolic', sql.Int, dia)
+                .input('pulse', sql.Int, pul)
+                .input('date', sql.DateTime, newMeasurement.date)
+                .query('INSERT INTO measurements (userId, systolic, diastolic, pulse, date) VALUES (@userId, @systolic, @diastolic, @pulse, @date)');
+            res.status(201).json({
+                message: 'המדידה נשמרה',
+                measurement: newMeasurement
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'שגיאה בשמירה' });
+        }
